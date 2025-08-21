8474e5856645d68db4557ce895eb35c6f17f4184
Đinh Mạnh Hữu <121842045+huud0905@users.noreply.github.com>
2025-08-21 12:46:17 +0700

    Update Locket.js

--- js/v2.0/Locket/Locket.js
+++ js/v2.0/Locket/Locket.js
@@ -16,14 +16,14 @@ var locket02 = {
   expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "2025-01-01T01:04:18Z",
+  original_purchase_date: "2024-07-28T01:04:18Z",
   purchase_date: "2024-07-28T01:04:17Z",
   store: "app_store"
 };
 
-var dohungx = {
+var huud = {
   grace_period_expires_date: null,
-  purchase_date: "2024-07-28T01:04:17Z",
+  purchase_date: "2025-01-01T01:04:17Z",
   product_identifier: "com.locket02.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z"
 };
@@ -34,16 +34,16 @@ if (match) {
   let [e, s] = mapping[match];
   
   if (s) {
-    dohungx.product_identifier = s;
+    huud.product_identifier = s;
     obj.subscriber.subscriptions[s] = locket02;
   } else {
     obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
   }
 
-  obj.subscriber.entitlements[e] = dohungx;
+  obj.subscriber.entitlements[e] = huud;
 } else {
   obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
-  obj.subscriber.entitlements.pro = dohungx;
+  obj.subscriber.entitlements.pro = huud;
 }
 
 $done({ body: JSON.stringify(obj) });
