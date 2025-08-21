d6ff8adef66690c98f37d8d93abf2f15a64e4855
Đinh Mạnh Hữu <121842045+huud0905@users.noreply.github.com>
2025-08-21 13:58:33 +0700

    Update Locket.js

--- js/v2.0/Locket/Locket.js
+++ js/v2.0/Locket/Locket.js
@@ -1,57 +1,48 @@
-// Locket.js - Bản 2.0, ngày tháng đổi sang 2025-01-02
-if (typeof $response !== 'undefined') {
-  try {
-    var mapping = {
-      '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-      'Locket': ['Gold']
-    };
+// ========= ID ========= //
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
+};
 
-    var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
-    var obj = JSON.parse($response.body || "{}");
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+var obj = JSON.parse($response.body);
 
-    obj.Attention = "Chúc mừng bạn! Bản 2.0 - Premium Active!";
+obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
 
-    var premiumSub = {
-      is_sandbox: false,
-      ownership_type: "PURCHASED",
-      billing_issues_detected_at: null,
-      period_type: "normal",
-      expires_date: "2099-12-18T01:04:17Z",
-      grace_period_expires_date: null,
-      unsubscribe_detected_at: null,
-      original_purchase_date: "2025-01-02T01:04:18Z",
-      purchase_date: "2025-01-02T01:04:17Z",
-      store: "app_store"
-    };
+var ohoang7 = {
+  is_sandbox: false,
+  ownership_type: "PURCHASED",
+  billing_issues_detected_at: null,
+  period_type: "normal",
+  expires_date: "2099-12-18T01:04:17Z",
+  grace_period_expires_date: null,
+  unsubscribe_detected_at: null,
+  original_purchase_date: "2025-01-02T01:04:18Z",
+  purchase_date: "2025-01-02T01:04:17Z",
+  store: "app_store"
+};
 
-    var premiumEnt = {
-      grace_period_expires_date: null,
-      purchase_date: "2025-01-02T01:04:17Z",
-      product_identifier: "com.locket02.premium.yearly",
-      expires_date: "2099-12-18T01:04:17Z"
-    };
+var huud = {
+  grace_period_expires_date: null,
+  purchase_date: "2025-01-02T01:04:17Z",
+  product_identifier: "com.ohoang7.premium.yearly",
+  expires_date: "2099-12-18T01:04:17Z"
+};
 
-    const match = Object.keys(mapping).find(e => ua.includes(e));
+const match = Object.keys(mapping).find(e => ua.includes(e));
 
-    if (match) {
-      let [e, s] = mapping[match];
-      if (s) {
-        premiumEnt.product_identifier = s;
-        obj.subscriber.subscriptions[s] = premiumSub;
-      } else {
-        obj.subscriber.subscriptions["com.locket02.premium.yearly"] = premiumSub;
-      }
-      obj.subscriber.entitlements[e] = premiumEnt;
-    } else {
-      obj.subscriber.subscriptions["com.locket02.premium.yearly"] = premiumSub;
-      obj.subscriber.entitlements.pro = premiumEnt;
-    }
-
-    console.log("[Locket v2.0] ✅ Premium injected!");
-    $done({ body: JSON.stringify(obj) });
-
-  } catch (e) {
-    console.log("[Locket v2.0] ❌ Error:", e);
-    $done({});
+if (match) {
+  let [e, s] = mapping[match];
+  if (s) {
+    huud.product_identifier = s;
+    obj.subscriber.subscriptions[s] = ohoang7;
+  } else {
+    obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = ohoang7;
   }
+  obj.subscriber.entitlements[e] = huud;
+} else {
+  obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = ohoang7;
+  obj.subscriber.entitlements.pro = huud;
 }
+
+$done({ body: JSON.stringify(obj) });
