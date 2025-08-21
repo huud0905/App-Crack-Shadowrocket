594655565aafc8e19bfe878d12a4b281c52f617a
Đinh Mạnh Hữu <121842045+huud0905@users.noreply.github.com>
2025-08-21 13:47:41 +0700

    Update deleteHeader.js

--- js/v2.0/Locket/deleteHeader.js
+++ js/v2.0/Locket/deleteHeader.js
@@ -1,37 +1,16 @@
-// DeleteHeader.js
-const version = 'v2.0';
-console.log(`[DeleteHeader] Script version: ${version}`);
+// DeleteHeader.js - từ v1.0 gốc, chỉ xóa header
+if (typeof $request !== 'undefined') {
+  let headers = $request.headers || {};
+  console.log("[DeleteHeader] Original Headers:", JSON.stringify(headers, null, 2));
 
-try {
-    let headers = $request.headers;
-    console.log("[DeleteHeader] Original Headers:", JSON.stringify(headers, null, 2));
-
-    // Danh sách header cần xóa/cập nhật
-    const headersToModify = {
-        "X-RevenueCat-ETag": "",
-        "If-None-Match": "",
-        "Cache-Control": "no-cache",
-        "Pragma": "no-cache"
-    };
-
-    // Hàm sửa header
-    function setHeaderValue(headers, key, value) {
-        const lowerKey = key.toLowerCase();
-        if (lowerKey in headers) {
-            headers[lowerKey] = value;
-        } else {
-            headers[key] = value;
-        }
+  let target = ["x-revenuecat-etag", "if-none-match", "cache-control", "pragma"];
+  for (let k of target) {
+    let key = Object.keys(headers).find(h => h.toLowerCase() === k);
+    if (key) {
+      delete headers[key];
+      console.log(`[DeleteHeader] Removed header: ${key}`);
     }
+  }
 
-    for (let key in headersToModify) {
-        setHeaderValue(headers, key, headersToModify[key]);
-    }
-
-    console.log("[DeleteHeader] Modified Headers:", JSON.stringify(headers, null, 2));
-    $done({ headers });
-
-} catch (err) {
-    console.log("[DeleteHeader] Error:", err);
-    $done({});
+  $done({ headers });
 }
