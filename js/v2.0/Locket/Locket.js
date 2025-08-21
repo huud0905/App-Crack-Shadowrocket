048da4b6faef79983ec76018258ff16baa5eb875
Đinh Mạnh Hữu <121842045+huud0905@users.noreply.github.com>
2025-08-21 13:05:41 +0700

    Update Locket.js

--- js/v2.0/Locket/Locket.js
+++ js/v2.0/Locket/Locket.js
@@ -16,8 +16,8 @@ var locket02 = {
   expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "2024-07-28T01:04:18Z",
-  purchase_date: "2024-07-28T01:04:17Z",
+  original_purchase_date: "2025-01-01T01:04:18Z",
+  purchase_date: "2025-01-01T01:04:17Z",
   store: "app_store"
 };
 
